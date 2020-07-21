c50a91fb1850dc30bd2add1b65e8cc6789241d0f
Mori01231 <mori01231mc@gmail.com>
2020-07-22 01:41:41 +0900

    [Add] ItemBanCommand and InventoryClickEvent

---
+++ src/main/java/com/github/mori01231/itemban/CommandExecutor/ItemBanCommand.java
@@ -0,0 +1,12 @@
+package com.github.mori01231.itemban.CommandExecutor;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ItemBanCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
--- src/main/java/com/github/mori01231/itemban/ItemBan.java
+++ src/main/java/com/github/mori01231/itemban/ItemBan.java
@@ -1,17 +1,44 @@
 package com.github.mori01231.itemban;
 
+import com.github.mori01231.itemban.CommandExecutor.ItemBanCommand;
+import com.github.mori01231.itemban.Listener.InventoryClickEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ItemBan extends JavaPlugin {
 
+    private static ItemBan instance;
+
+    public ItemBan (){
+        instance = this;
+    }
+
+    public static ItemBan getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("LifeCore has been enabled.");
+        this.getCommand("itemban").setExecutor(new ItemBanCommand());
 
+
+
+        this.saveDefaultConfig();
+
+        registerEvents();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void registerEvents(){
+
+        PluginManager pm = getServer().getPluginManager();
+
+        pm.registerEvents(new InventoryClickEvent(this),  this);
     }
 }
