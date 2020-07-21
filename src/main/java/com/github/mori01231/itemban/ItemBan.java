d712b2db0ea374fe00b319e8e45ac9bc3618a5b4
Mori01231 <mori01231mc@gmail.com>
2020-07-22 01:26:57 +0900

    [Add] Initial commit

---
+++ src/main/java/com/github/mori01231/itemban/ItemBan.java
@@ -0,0 +1,17 @@
+package com.github.mori01231.itemban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemBan extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
