d8b4291cf6e6efebbebfff0f1835de2b74a0c67b
Mori01231 <mori01231mc@gmail.com>
2020-07-23 02:03:13 +0900

    [Add] Function to delete only certain armor.

--- src/main/java/com/github/mori01231/itemban/Listener/InventoryEventListener.java
+++ src/main/java/com/github/mori01231/itemban/Listener/InventoryEventListener.java
@@ -42,15 +42,34 @@ public class InventoryEventListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onInventoryCloseEvent(InventoryCloseEvent event) {
+        Inventory inv = event.getInventory();
         String playerName = event.getPlayer().getName();
 
         ItemStack OffHandItem = event.getPlayer().getInventory().getItemInOffHand();
 
+
+        // Delete OffHand Item
         try{
             for (String line : ItemBan.getInstance().getConfig().getStringList("BannedItems.OffHand")) {
                 if(OffHandItem.getItemMeta().getDisplayName().equals(line)){
                     OffHandItem.setAmount(0);
-                    Log("&3" + playerName + "の所持する" + line + "が削除されました。");
+                    Log("&3" + playerName + "の所持するオフハンドアイテム：" + line + "が削除されました。");
+                }
+            }
+        }catch (Exception e){
+        }
+
+        // Delete Armor Item
+        try{
+            for (ItemStack item: event.getPlayer().getInventory().getArmorContents()) {
+                try{
+                    for (String line : ItemBan.getInstance().getConfig().getStringList("BannedItems.Armor")) {
+                        if(item.getItemMeta().getDisplayName().equals(line)){
+                            item.setAmount(0);
+                            Log("&3" + playerName + "の所持する防具：" + line + "が削除されました。");
+                        }
+                    }
+                }catch (Exception e){
                 }
             }
         }catch (Exception e){
