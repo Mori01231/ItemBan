9d5aeb89aba20b122fdf254c19a935095f047d98
Mori01231 <mori01231mc@gmail.com>
2020-07-23 01:50:37 +0900

    [Add] Removes offhand items.

--- src/main/java/com/github/mori01231/itemban/Listener/InventoryEventListener.java
+++ src/main/java/com/github/mori01231/itemban/Listener/InventoryEventListener.java
@@ -3,9 +3,11 @@ package com.github.mori01231.itemban.Listener;
 
 import com.github.mori01231.itemban.ItemBan;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -29,6 +31,25 @@ public class InventoryEventListener implements Listener {
             try{
                 for (String line : ItemBan.getInstance().getConfig().getStringList("BannedItems.All")) {
                     if(item.getItemMeta().getDisplayName().equals(line)){
+                        item.setAmount(0);
+                        Log("&3" + playerName + "の所持する" + line + "が削除されました。");
+                    }
+                }
+            }catch (Exception e){
+            }
+        }
+    }
+
+    public void onInventoryCloseEvent(InventoryCloseEvent event) {
+        Inventory inv = event.getInventory();
+        String playerName = event.getPlayer().getName();
+
+        ItemStack OffHandItem = event.getPlayer().getInventory().getItemInOffHand();
+
+        for (ItemStack item: inv.getContents()) {
+            try{
+                for (String line : ItemBan.getInstance().getConfig().getStringList("BannedItems.OffHand")) {
+                    if(OffHandItem.getItemMeta().getDisplayName().equals(line)){
                         item.setAmount(0);
                         Log("&3" + playerName + "の所持する" + line + "が削除されました。");
                     }
