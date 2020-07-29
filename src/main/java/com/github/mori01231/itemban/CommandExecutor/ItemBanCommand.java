99ef06cb1a0e74b7eed40aadcd7bc6f24d981a63
Mori01231 <mori01231mc@gmail.com>
2020-07-30 00:53:02 +0900

    [Add] Get item in main hand.

--- src/main/java/com/github/mori01231/itemban/CommandExecutor/ItemBanCommand.java
+++ src/main/java/com/github/mori01231/itemban/CommandExecutor/ItemBanCommand.java
@@ -3,10 +3,17 @@ package com.github.mori01231.itemban.CommandExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
 
 public class ItemBanCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        Player player = (Player) sender;
+
+        ItemStack mainHandItem = player.getInventory().getItemInMainHand();
+
+        return true;
     }
 }
