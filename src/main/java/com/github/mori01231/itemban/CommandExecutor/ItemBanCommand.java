1d41d92f9e0bc6da557a2ca2531d5175afa47aed
Mori01231 <mori01231mc@gmail.com>
2020-07-31 00:33:58 +0900

    [Add] Command to add item to list of banned items.

--- src/main/java/com/github/mori01231/itemban/CommandExecutor/ItemBanCommand.java
+++ src/main/java/com/github/mori01231/itemban/CommandExecutor/ItemBanCommand.java
@@ -1,18 +1,27 @@
 package com.github.mori01231.itemban.CommandExecutor;
 
+import com.github.mori01231.itemban.ItemBan;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+
 
 public class ItemBanCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         Player player = (Player) sender;
 
         ItemStack mainHandItem = player.getInventory().getItemInMainHand();
+
+        List<String> BannedItems = ItemBan.getInstance().getConfig().getStringList("BannedItems.All");
+        BannedItems.add(mainHandItem.getItemMeta().getDisplayName());
+
+        ItemBan.getInstance().getConfig().set("BannedItems.All", BannedItems);
 
         return true;
     }
