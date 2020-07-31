79a3b044dfb6194fbf5b5b339dd24ff930b7ccad
Mori01231 <mori01231mc@gmail.com>
2020-08-01 02:10:45 +0900

    [Add] ItemReplaceCommand executor.

--- src/main/java/com/github/mori01231/itemban/ItemBan.java
+++ src/main/java/com/github/mori01231/itemban/ItemBan.java
@@ -1,6 +1,7 @@
 package com.github.mori01231.itemban;
 
 import com.github.mori01231.itemban.CommandExecutor.ItemBanCommand;
+import com.github.mori01231.itemban.CommandExecutor.ItemReplaceCommand;
 import com.github.mori01231.itemban.Listener.InventoryEventListener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -22,6 +23,7 @@ public final class ItemBan extends JavaPlugin {
         // Plugin startup logic
         getLogger().info("ItemBan has been enabled.");
         this.getCommand("itembanadd").setExecutor(new ItemBanCommand());
+        this.getCommand("itemreplaceadd").setExecutor(new ItemReplaceCommand());
 
         this.saveDefaultConfig();
 
