71de0cb9cb656af49319789ed772e2a02096c4f5
Mori01231 <mori01231mc@gmail.com>
2020-08-02 01:18:25 +0900

    [Add] ReplaceItem function

--- src/main/java/com/github/mori01231/itemban/CommandExecutor/ItemReplaceCommand.java
+++ src/main/java/com/github/mori01231/itemban/CommandExecutor/ItemReplaceCommand.java
@@ -1,12 +1,20 @@
 package com.github.mori01231.itemban.CommandExecutor;
 
+import com.github.mori01231.itemban.ItemBan;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
 
 public class ItemReplaceCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
+    }
+
+    public void ReplaceItem(String ReplaceItem, String ReplacedItem){
+
+        // Save replacement item.
+        ItemBan.getInstance().getConfig().set(ReplaceItem, ReplacedItem);
     }
 }
