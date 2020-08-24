f7fd4dc8ffa37aac2a0bffc1cea53eeb0c89a850
Mori01231 <mori01231mc@gmail.com>
2020-08-24 14:30:20 +0900

    [Refactor] Added comments.

--- src/main/java/com/github/mori01231/itemban/CommandExecutor/ReloadCommand.java
+++ src/main/java/com/github/mori01231/itemban/CommandExecutor/ReloadCommand.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nonnull;
+
 import static org.bukkit.Bukkit.getLogger;
 
 public class ReloadCommand implements CommandExecutor {
@@ -15,26 +17,32 @@ public class ReloadCommand implements CommandExecutor {
     private Boolean isConsole;
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String label, @Nonnull String[] args) {
 
+        // Check if the sender is the console or a player
         if (sender instanceof Player) {
-            isConsole = false;
+            isConsole = false; // Command was executed by a player
             player = (Player) sender; // Get the player
         }else{
-            isConsole = true;
+            isConsole = true; // Command was executed by console
         }
 
+        // Reload the config
         ItemBan.getInstance().reloadConfig();
 
+        // Give feedback indicating the command was properly executed
         FeedBack("&3正常にリロードされました。");
 
         return true;
     }
 
     public void FeedBack(String message){
+        // Send the feedback to different places depending on who the sender is
         if(isConsole){
+            // send feedback to the console
             getLogger().info(ChatColor.translateAlternateColorCodes('&',message));
         }else{
+            // send feedback to the player
             player.sendMessage(ChatColor.translateAlternateColorCodes('&',message));
         }
     }
