5ca4df6c96b5cd646d00626e464aac7eb02a9238
Mori01231 <mori01231mc@gmail.com>
2020-08-24 14:20:29 +0900

    [Add] Function to efficiently provide feedback.

--- src/main/java/com/github/mori01231/itemban/CommandExecutor/ReloadCommand.java
+++ src/main/java/com/github/mori01231/itemban/CommandExecutor/ReloadCommand.java
@@ -1,12 +1,36 @@
 package com.github.mori01231.itemban.CommandExecutor;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static org.bukkit.Bukkit.getLogger;
 
 public class ReloadCommand implements CommandExecutor {
+
+    private Player player;
+    private Boolean isConsole;
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+
+        if (sender instanceof Player) {
+            isConsole = false;
+            player = (Player) sender; // Get the player
+        }else{
+            isConsole = true;
+        }
+
+        return true;
+    }
+
+    public void FeedBack(String message){
+        if(isConsole){
+            getLogger().info(ChatColor.translateAlternateColorCodes('&',message));
+        }else{
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',message));
+        }
     }
 }
