b0a859de4bf0399e54cfb65cd271c16fd49387b9
Mori01231 <mori01231mc@gmail.com>
2020-08-28 15:23:42 +0900

    [Refactor] Removed unused import

--- src/main/java/com/github/mori01231/itemban/CommandExecutor/ReloadCommand.java
+++ src/main/java/com/github/mori01231/itemban/CommandExecutor/ReloadCommand.java
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import javax.annotation.Nonnull;
 
 import static org.bukkit.Bukkit.getLogger;
 
